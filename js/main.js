0f40b231222287763bb22db1db38998f776decc3
Julio Acuña <urkonn@gmail.com>
2014-05-20 19:45:05 -0500

    cambio id google form

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@ $( document ).ready(function() {
   $('form#signup').on('submit', function(e) {
     e.preventDefault();
     $.ajax({
-      url: 'https://docs.google.com/forms/d/1e9YVlCUhFD3eRGX2gFIn35mIMkD90HSh9fKeZfgY6A8/formResponse?embedded=true',
+      url: 'https://docs.google.com/forms/d/1K_CPkixxzmkQy0IQekA6-XK-i8Pf0zU8UvSUJYqmHAA/formResponse?embedded=true', 
       type: 'POST',
-      data: {
-        'entry.1624144845': $('input#name').val(),
-        'entry.987085994':  $('input#email').val(),
-        'entry.1924100915': $('input#company').val(),
-        'entry.1530080620': $('input#url').val()
+      data: {        
+        'entry.1117179829': $('input#name').val(),
+        'entry.1518802305': $('input#institucion').val(),
+        'entry.203672258': $('input#email').val(),
+        'entry.910027698': $('input#pais').val(),
+        'entry.1520415052': $('input#sector').val(),
+        'entry.1520415052.other_option_response': $('input#otrosector').val(),
+        'entry.1513088725': $('input#preguntas').val(),
       },
       dataType: 'jsonp',
       complete: function() {
