ef84835f2066170078f37abe4e284e00d2ee7af5
Rod Wilhelmy <rwilhelmy@gmail.com>
2014-05-20 22:33:40 -0500

    Fixes radio value extraction

--- js/main.js
+++ js/main.js
@@ -1,30 +1,41 @@
-$( document ).ready(function() {
+
+function showSuccessWithMessage(msg) {
+  $(".alert").text(msg).addClass('alert-success').show();
+}
+
+function submitToGoogleForm() {
+  var sectorValue = $("input[name=sector]:checked").val();
+  var otherSectorValue = ''
+  if (sectorValue === "Otro")
+    otherSectorValue = $("input[name=other-sector]").val();
+
+  $.ajax({
+    url: 'https://docs.google.com/forms/d/1K_CPkixxzmkQy0IQekA6-XK-i8Pf0zU8UvSUJYqmHAA/formResponse?embedded=true',
+    type: 'POST',
+    data: {
+      'entry.1117179829': $('input#name').val(),
+      'entry.1518802305': $('input#institucion').val(),
+      'entry.203672258': $('input#email').val(),
+      'entry.910027698': $('input#pais').val(),
+      'entry.1520415052': sectorValue,
+      'entry.1520415052.other_option_response': otherSectorValue,
+      'entry.1513088725': $('input#preguntas').val(),
+    },
+    dataType: 'jsonp',
+    complete: function() {
+      // We are using the 'complete' callback, instead of 'success' or 'error'
+      // because we haven't figured out the following error:
+      // Uncaught SyntaxError: Unexpected token < formResponse:1
+      showSuccessWithMessage('¡Gracias por suscribirte! Te mantendremos al tanto.');
+    }
+  });
+}
+
+$(document).ready(function() {
   $('form#signup').on('submit', function(e) {
     e.preventDefault();
-    $.ajax({
-      url: 'https://docs.google.com/forms/d/1K_CPkixxzmkQy0IQekA6-XK-i8Pf0zU8UvSUJYqmHAA/formResponse?embedded=true', 
-      type: 'POST',
-      data: {        
-        'entry.1117179829': $('input#name').val(),
-        'entry.1518802305': $('input#institucion').val(),
-        'entry.203672258': $('input#email').val(),
-        'entry.910027698': $('input#pais').val(),
-        'entry.1520415052': $('input#sector').val(),
-        'entry.1520415052.other_option_response': $('input#otrosector').val(),
-        'entry.1513088725': $('input#preguntas').val(),
-      },
-      dataType: 'jsonp',
-      complete: function() {
-        // We are using the 'complete' callback, instead of 'success' or 'error'
-        // because we haven't figured out the following error:
-        // Uncaught SyntaxError: Unexpected token < formResponse:1
-        $(".alert").text('¡Gracias por registrarte! Te mantendremos al tanto.');
-        $(".alert").addClass('alert-success');
-        $(".alert").show();
-        //$('button[type="submit"]').attr('disabled', 'disabled');
-        //$('button[type="submit"]').addClass('disabled');
-      }
-    });
+    submitToGoogleForm();
   });
 });
+
 
--- js/valida.js
+++ js/valida.js
@@ -1,28 +1,27 @@
 $(document).ready(function () {
-    $("form").validate({
-        showErrors: function (errorMap, errorList) {
+  $("form#signup").validate({
+    showErrors: function (errorMap, errorList) {
+      // Clean up any tooltips for valid elements
+      $.each(this.validElements(), function (index, element) {
+        var $element = $(element);
 
-            // Clean up any tooltips for valid elements
-            $.each(this.validElements(), function (index, element) {
-                var $element = $(element);
+        // Clear the title - there is no error associated anymore
+        $element.data("title", "")
+        .removeClass("error")
+        .tooltip("destroy");
+      });
 
-                $element.data("title", "") // Clear the title - there is no error associated anymore
-                .removeClass("error")
-                    .tooltip("destroy");
-            });
-
-            // Create new tooltips for invalid elements
-            $.each(errorList, function (index, error) {
-                var $element = $(error.element);
-
-                $element.tooltip("destroy") // Destroy any pre-existing tooltip so we can repopulate with new tooltip content
-                .data("title", error.message)
-                    .addClass("error")
-                    .tooltip(); // Create a new tooltip based on the error messsage we just set in the title
-            });
-        },
-        submitHandler: function (form) {
-            form.submit();
-        }
-    });
+      // Create new tooltips for invalid elements
+      $.each(errorList, function (index, error) {
+        var $element = $(error.element);
+        // Destroy any pre-existing tooltip so we can repopulate with new tooltip content
+        // and create a new tooltip based on the error messsage we just set in the title
+        $element.tooltip("destroy")
+          .data("title", error.message)
+          .addClass("error")
+          .tooltip();
+      });
+    },
+    submitHandler: function (form) { form.submit() }
+  });
 });
