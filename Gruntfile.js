b340fc4814dae47b851c0e239df82deafdc05a42
Rod Wilhelmy <rwilhelmy@gmail.com>
2014-09-23 13:35:34 -0500

    Merges everything into one main.js using Grunt
    * Adds Gruntfile.js
    * Use `grunt default` to concatenate all js (including bower components)
    into one main.js
    * Mueve javascript general a condatos.js

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bower_concat: {
+      all: {
+        dest: 'js/_bower.js',
+        bowerOptions: {
+          relative: false
+        }
+      }
+    },
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['js/**/*.js'],
+        // the location of the resulting JS file
+        dest: 'js/main.js'
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'js/<%= pkg.name %>.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'js/main.js',
+        dest: 'js/main.js'
+      }
+    },
+    clean: {
+      js: ["js/_bower.js"]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'bower_concat', 'concat', 'uglify', 'clean']);
+
+};
+
---
+++ js/condatos.js
@@ -0,0 +1,61 @@
+/**
+** pequeña función para animar scroll
+*/
+$(document).ready(function() {
+	
+	$('.popoverable').popover({});
+	
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  
+/**
+** para compartir en tw
+*/
+  $("a.tw").on("click", function(){
+	    var width  = 575,
+        height = 400,
+        left   = ($(window).width()  - width)  / 2,
+        top    = ($(window).height() - height) / 2,
+        url    = this.href,
+        opts   = 'status=1' +
+                 ',width='  + width  +
+                 ',height=' + height +
+                 ',top='    + top    +
+                 ',left='   + left;
+    
+    window.open(url, 'twitter', opts);
+ 
+    return false;
+  });
+
+/**
+** para compartir en fb
+*/  
+  $('a.fb').on("click", function(){
+	    var width  = 575,
+        height = 400,
+        left   = ($(window).width()  - width)  / 2,
+        top    = ($(window).height() - height) / 2,
+        url    = this.href,
+        opts   = 'status=1' +
+                 ',width='  + width  +
+                 ',height=' + height +
+                 ',top='    + top    +
+                 ',left='   + left;
+    
+    window.open(url, 'facebook', opts);
+ 
+    return false;
+  });
+  
+});
