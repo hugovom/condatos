4444bc06e443e896b99ecc40871771626e871018
Rod Wilhelmy <rwilhelmy@gmail.com>
2014-09-24 13:43:01 -0500

    Quita timestamp a main.js
    Argumentos:
    * Generará commit changes por el simple hecho de compilar en otro día
    * No es necesario ya que Git ya nos sirve para controlar las versiones

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,9 +43,6 @@ module.exports = function(grunt) {
       }
     },
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
       build: {
         src: 'js/main.js',
         dest: 'js/main.js'
